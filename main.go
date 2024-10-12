2184c299749ab722925602facbc93878e2d5093c
Franco Ferraguti <ferra.main@gmail.com>
2024-10-12 15:47:04 -0300

    :)

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+)
+
+func main() {
+	// This program calls fnWithDeferredGoroutine, which on defer spins up a goroutine that sleeps 1s and then prints '2'.
+	// The main func will continue executing while the goroutine is sleeping, so it will print '1' before the '2' is printed.
+	// After printing '1', the main func also sleeps for a while, giving the goroutine time to wake up and print the '2' before the program exits.
+	// This will output: 012
+
+	print("0")
+	fnWithDeferredGoroutine()
+	print("1")
+	time.Sleep(2000) // If we remove this, then the '2' won't get printed as the program will exit and stop running goroutines.
+}
+
+func fnWithDeferredGoroutine() {
+	defer func() {
+		go func() {
+			time.Sleep(1000)
+			print("2")
+		}()
+	}()
+}
